6ff5cf142b9e0ad9072b7a59707e50d9dc6a005d
iioufogood <2473191827@qq.com>
2018-07-29 18:34:33 +0800

    vue-test commit

---
+++ vue-playlist/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <el-switch
+      v-model="value2"
+      active-color="#13ce66"
+      inactive-color="#ff4949">
+    </el-switch>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      value2: true
+    };
+  },
+  watch: {
+    value2(newValue) {
+      console.log(newValue);
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
